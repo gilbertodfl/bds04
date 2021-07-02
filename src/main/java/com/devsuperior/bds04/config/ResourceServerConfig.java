de0fa4af402478e4b65a990501f80e424fb6c998
Gilberto de Freitas Lins <gilbertodefreitaslins@hotmail.com>
2021-07-02 20:44:47 -0300

    Finish Work - Trabalho final

--- src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
@@ -21,16 +21,24 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	@Autowired
 	private JwtTokenStore tokenStore;
-	// aqui temos que trocar por cities e events 
-/*	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" , "/events/", "/cities/"};
-	private static final String[] OPERATOR_OR_ADMIN = { "/events/**", "/cities/**" };
-	private static final String[] ADMIN = { "/users/**" };	
-*/	
+
+	/*********************************************************************************************
+		1) somente as rotas de leitura (GET) de eventos e cidades são públicas (não precisa de login). 
+		2) Usuários CLIENT podem também inserir (POST) novos eventos. 
+		3) Os demais acessos são permitidos apenas a usuários ADMIN.
+		4) Validações de City:
+			 Nome não pode ser vazio
+		5) Validações de Event:
+			 Nome não pode ser vazio
+			 Data não pode ser passada
+			 Cidade não pode ser nula	
+   **********************************************************************************************/
+
 	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	private static final String[] GETS_PUBLIC = {  "/events/**", "/cities/**" };
 	private static final String[] POST_LOG = {  "/events/**" };
-	
 	private static final String[] ADMIN = { "/users/**" ,"/cities/**"};
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
@@ -39,27 +47,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 
-		// H2
+		// Banco H2 - liberando os frames de suas telas. 
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-//		.antMatchers(HttpMethod.POST, POST_LOG).permitAll()
 		.antMatchers(HttpMethod.GET, GETS_PUBLIC).permitAll()
 		.antMatchers(POST_LOG).hasAnyRole("CLIENT", "ADMIN")
 		.antMatchers(ADMIN).hasRole("ADMIN")
 		.anyRequest().authenticated();
-/*		
-		http.authorizeRequests()
-		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, PUBLIC).permitAll()
-		.antMatchers(HttpMethod.POST, PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
-		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("CLIENT", "ADMIN")
-		.antMatchers(ADMIN).hasRole("ADMIN")
-		.anyRequest().authenticated();
-	*/	 
 	}	
 }
--- src/main/java/com/devsuperior/bds04/controllers/exceptions/ValidationError.java
+++ src/main/java/com/devsuperior/bds04/controllers/exceptions/ValidationError.java
@@ -3,9 +3,26 @@ package com.devsuperior.bds04.controllers.exceptions;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.services.exceptions.ResourceNotFoundException;
+
 public class ValidationError extends StandardError {
 	private static final long serialVersionUID = 1L;
 
+	/* A lógica aqui é assim: 
+	 * Para que as mensagens saiam tabelas, criamos a Classe Field message, que possui campo e descrição. 
+	 * Aqui "instanciamos" os fields com errors
+	 * Observe que ele é um EXTENDS do StandError, logo só precisamos tratar o getErrros e o addError. 
+	 * Veja também: O [City/Event]Service.java chama o  ResourceExceptionsHandler.java
+	 * Por exemplo: 
+	 * O chama  CityService.java chama :City entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+	 *  que por sua vez: chama o ResourceExceptionsHandler.java
+	 *  	@ExceptionHandler( ResourceNotFoundException.class )
+
+	 *  que por sua vez chamaria aqui, mas neste projeto não fizemos isto.  
+	 */
 	private List<FieldMessage> errors = new ArrayList<>();
 	// observe que não tem construct porque herdou da classe StandarError que 
 	// já possui. 
